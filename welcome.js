ff91fa3055387e162eeeeb802c837feb1d2858ea
ThatsWhatYouGit <dan@vagg.org>
2023-11-09 23:48:04 +1100

    Included a Message

---
+++ welcome.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+console.log("Hello from local");
+\
+/*
+
+git clone <remote -http://>
+
+git add filename.extention - add file
+git commit -m "message" - commit a file
+
+*/
