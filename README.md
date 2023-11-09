41e978ddf807463d723e6c53b43aace786a22126
ThatsWhatYouGit <dan@vagg.org>
2023-11-10 00:30:29 +1100

    all files added

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Learning git
 
 Add this in remote
+
+This is to explain project folder
--- welcome.js
+++ welcome.js
@@ -10,4 +10,8 @@ git add filename.extention - add file
 git commit -m "message" - commit a file
 
 git push or git push origin - to send your file to origin/remote
+
+git branch branchname
+git checkout - to navigate between branches
+git checkout -b newbranchName
 */
