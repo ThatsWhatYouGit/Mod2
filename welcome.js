3d5719fe47bad5f242f6ffe5b3508c5b7b252d4e
ThatsWhatYouGit <dan@vagg.org>
2023-11-10 00:06:19 +1100

    Just added

--- welcome.js
+++ welcome.js
@@ -1,7 +1,7 @@
 console.log("hello world");
 
 console.log("Hello from local");
-\
+
 /*
 
 git clone <remote -http://>
@@ -9,4 +9,5 @@ git clone <remote -http://>
 git add filename.extention - add file
 git commit -m "message" - commit a file
 
+git push or git push origin - to send your file to origin/remote
 */
